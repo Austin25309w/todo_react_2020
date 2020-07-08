e3ab930079ad07d7e7535cedbc8f9537aa005cfb
Austin25309w <austinow@gmail.com>
2020-07-08 16:54:37 -0700

    created R operation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import List from './components/List'
 import './App.css';
 
-function App() {
+class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+        show: false
+    }
+};
+render(){
+
   return (
     <div className="App">
       <header className="App-header">
        <h1>Todo</h1>
-       <List/>
+       <List name = "Austin" />
       </header>
     </div>
   );
+}
 }
 
 export default App;
--- src/components/List.js
+++ src/components/List.js
@@ -1,17 +1,71 @@
 import React from 'react';
+// import ReactDOM from 'react-dom';
 
-function List(props) {
-    const items = [];
-    const submitHandler = (e) =>{
-        e.preventDefault();
 
-    } 
-    return <>
-    <form onSubmit ={submitHandler}> 
-        <input ></input>
-        <button >Add an Item</button>
-    </form>
-    </>
+class List extends React.Component {
+    
+    state = {
+        edit: false,
+        items: [
+            {
+            id: '1', title: 'Buy Milk', done: false, date: new Date()},
+            {id: '2', title: 'Meeting with Ali', done: false, date: new Date()},
+            {id: '3', title: 'Tea break', done: false, date: new Date()},
+            {id: '4', title: 'Go for a run.', done: false, date: new Date()},
+        ],
+        inputVal : '', 
+        id: null
+    };
+    
+    // add() {
+    //     const {items, inputVal} = this.state;
+    //     if(inputVal){
+    //         const nextState = [...items, inputVal]
+    //         this.setState({ items: nextState, inputVal: ''})
+    //     }
+    //     this.setState({
+    //         items: []
+    //     })
+    // }
+
+    
+    // submitHandler = (e) => {
+    //     e.preventDefault();
+
+    // } 
+
+    onSubmitHandle(e){
+
+    }
+
+    onEditHandle(e){
+
+    }
+
+    onDeleteHandle(e){
+
+    }
+    render(){
+        // const {inputVal} = this.state;
+    return (
+        <div>
+            <form onSubmit={this.onSubmitHandle.bind(this)}>
+                <input type="text" name="item" className="item"/>
+                <button className="btn-add-item">Add Item</button>
+            </form>
+            <ul style ={{ listStyleType: "none"}}>
+                {this.state.items.map(item => (
+                    <li key={item.id}>{item.title}
+                        <button onClick={this.onDeleteHandle.bind(this, item.id)}>Delete</button> 
+                        <button onClick={this.onEditHandle.bind(this, item.id, item.title)}>Edit</button>
+                        <button onClick={this.onCompleteHandle}>Complete</button>
+                    </li>
+                ))}
+            </ul>
+
+        </div>
+    )
+    }
 }
 
 export default List;
