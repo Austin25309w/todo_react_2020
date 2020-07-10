521063ee82e15b810cbf4c86210bfc503342832e
Austin25309w <austinow@gmail.com>
2020-07-09 19:02:49 -0700

    edit feature

--- src/components/List.js
+++ src/components/List.js
@@ -4,40 +4,47 @@ import React from 'react';
 
 class List extends React.Component {
     
-    state = {
-        edit: false,
-        items: [
-            {
-            id: '1', title: 'Buy Milk', done: false, date: new Date()},
-            {id: '2', title: 'Meeting with Ali', done: false, date: new Date()},
-            {id: '3', title: 'Tea break', done: false, date: new Date()},
-            {id: '4', title: 'Go for a run.', done: false, date: new Date()},
-        ],
-        inputVal : '', 
-        id: null
-    };
-    
-    // add() {
-    //     const {items, inputVal} = this.state;
-    //     if(inputVal){
-    //         const nextState = [...items, inputVal]
-    //         this.setState({ items: nextState, inputVal: ''})
-    //     }
-    //     this.setState({
-    //         items: []
-    //     })
-    // }
+    constructor(props){
+        super(props);
 
-    
-    // submitHandler = (e) => {
-    //     e.preventDefault();
-
-    // } 
-
-    
-    onEditHandle(e){
-        
+        this.state = {
+            edit: false,
+            id: null,
+            items: [
+                {id: '1', title: 'Buy Milk', done: false, date: new Date()},
+                {id: '2', title: 'Meeting with Ali', done: false, date: new Date()},
+                {id: '3', title: 'Tea break', done: false, date: new Date()},
+                {id: '4', title: 'Go for a run.', done: false, date: new Date()},
+            ],  
+            add: true
+        };
     }
+
+
+    onEditHandle(e){
+        this.setState({
+            edit: true,
+            add: false,
+            id: arguments[0],
+            title: arguments[1],
+        })
+    }
+
+    onUpdateHandle(e){
+        e.preventDefault();
+
+        this.setState({
+            items: this.state.items.map( item => {
+                if(item.id === this.state.id){
+                    item['title'] = e.target.updatedItem.value;
+                    return item;
+                }
+                return item;
+            })
+        });
+        this.setState({ edit: false });
+    }
+
     
     onDeleteHandle(){
         let id = arguments[0];
@@ -58,17 +65,29 @@ class List extends React.Component {
         e.target.item.value = '';
         
     }
+
+    renderEditForm(){
+        if(this.state.edit){
+
+            return <form onSubmit={this.onUpdateHandle.bind(this)}>
+                <input type="text" name="updatedItem" className="item" defaultValue={this.state.title} />
+                <button className="update-add-item">Update</button>
+            </form>
+        }
+    }
+
     render(){
-        // const {inputVal} = this.state;
     return (
         <div>
+            {this.renderEditForm()}
             <form onSubmit={this.onSubmitHandle.bind(this)}>
                 <input type="text" name="item" className="item"/>
                 <button className="btn-add-item">Add Item</button>
             </form>
             <ul style ={{ listStyleType: "none"}}>
                 {this.state.items.map(item => (
-                    <li key={item.id}>{item.title}
+                    <li key={item.id}>
+                        {item.title}
                         <button onClick={this.onDeleteHandle.bind(this, item.id)}>Delete</button> 
                         <button onClick={this.onEditHandle.bind(this, item.id, item.title)}>Edit</button>
                         <button onClick={this.onCompleteHandle}>Complete</button>
